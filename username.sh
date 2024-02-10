5241c133454d7e460fa24bb1e22c42306fb5f314
root <root@special-action-1.localdomain>
2024-02-10 00:15:31 -0800

    Update username.sh permissions to executable

--- username.sh
+++ username.sh
@@ -18,3 +18,17 @@ fi
 # 使用sed命令替换文件中的用户名
 sed -i'' "s/\"username\": \"[^\"]*\"/\"username\": \"$new_username\"/g" "$config_file"
 echo "Username has been updated to '$new_username' in the config file."
+
+cd sing-box || exit
+
+#运行install_go.sh脚本
+./release/local/install_go.sh
+
+#创建/var/lib/sing-box目录
+mkdir -p /var/lib/sing-box
+
+#运行install.sh脚本
+./release/local/install.sh
+
+#启用sing-box服务
+systemctl enable sing-box
